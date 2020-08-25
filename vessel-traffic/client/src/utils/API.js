16b83307a112a6cf5de3122e87643b5068dc28cb
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-24 18:27:56 -0700

    vessel photos

--- vessel-traffic/client/src/utils/API.js
+++ vessel-traffic/client/src/utils/API.js
@@ -1,27 +1,14 @@
 import axios from "axios";
 //the AREAURL is the endpoint url for marine traffic's PS05 predefined area search
 const AREAURL = "https://my-json-server.typicode.com/audijej/demo";
+const photoURL = "https://services.marinetraffic.com/api/exportvesselphoto";
 
-//Marine Traffic API url and key
-// const BASEURL = "https://services.marinetraffic.com/api/exportvessels/v:8/"
-// const APIKEY = "b2b4add846dd4b4b3b76a5db415135873d15304f";
 
-// const passenger = "/shiptype:6"
-// const cargo = "/shiptype:7"
-// const tanker = "/shiptype:8"
-
-//how can i combine this search to search ships 6-8 with one call?
 
 export default {
     searchShips: function() {
         return axios.get( AREAURL);
     },
 
-    // searchCargoShips: function() {
-    //     return axios.get(BASEURL + APIKEY + cargo);
-    // },
-
-    // searchTankers: function() {
-    //     return axios.get(BASEURL + APIKEY + tanker);
-    // }
+    
 };
