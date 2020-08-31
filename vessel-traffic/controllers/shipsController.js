c15af8df5b603f41904fe3a9b96a719cb3d996e6
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-31 00:50:53 -0700

    added controller functions

--- vessel-traffic/controllers/shipsController.js
+++ vessel-traffic/controllers/shipsController.js
@@ -60,3 +60,65 @@ router.put("/api/ships/:id", function(req, res) {
 });
 
 module.exports = router;
+
+
+//***********************************************/
+const { Op } = require("sequelize");
+const trip = require("../models/trip.js");
+
+// Find all users
+const users = await User.findAll();
+console.log(users.every(user => user instanceof User)); // true
+console.log("All users:", JSON.stringify(users, null, 2));
+
+//Find all trips
+const trips = await Trip.findAll();
+console.log(trips.every(trip => trip instanceof Trip));
+console.log("All trips:", JSON.stringify(trips, null, 2));
+
+//Find all ships and boats
+const ships = await Ship.findAll();
+console.log(ships.every(ship => ship instanceof Ship));
+console.log("All ships and boats:", JSON.stringify(trips, null, 2))
+
+//Find all large type ships
+Ship.findAll({
+  where: {
+    [Op.and]: [
+      { ship_type_name: 'Bulk Carrier' },
+      { ship_type_name: 'Ro-Ro/Passenger Ship' },
+      { ship_type_name: 'Container Ship' },
+      { ship_type_name: 'Inland, Passenger Ship, Ferry, Cruise ship'},
+      {ship_type_name: 'Passenger'}
+    ]
+  }
+});
+
+//create trip
+const tripDate = await Trip.create({
+  sail_date_id: '',
+  start_destination: '',
+  end_destination: '',
+  start_sail_date: '',
+  end_sail_date: ''
+}, {fields: ['start_destination', 'end_destination', 'start_sail_date', 'end_sail_date']})
+
+//update trip
+const updateTrip = 
+
+//delete trip
+const deleteTrip = await Post.destroy({
+  where: {
+    sail_date_id: '',
+  }
+});
+
+// const user = await User.create({
+//   username: 'alice123',
+//   isAdmin: true
+// }, { fields: ['username'] });
+// // let's assume the default of isAdmin is false
+// console.log(user.username); // 'alice123'
+// console.log(user.isAdmin); // false
+
+
--- vessel-traffic/models/index.js
+++ vessel-traffic/models/index.js
@@ -8,12 +8,12 @@ const sequelize = new Sequelize("sound_vessel_traffic_db", "root", "passwordgoes
 } );
 //Sequelize refers to the library itself while sequelize refers to an instance of 
 //Sequelize, which represents a connection to one database
-// try {
-//     await sequelize.authenticate();
-//     console.log('Sequelize connection has been established successfully');
-// }   catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
+try {
+    await sequelize.authenticate();
+    console.log('Sequelize connection has been established successfully');
+}   catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
